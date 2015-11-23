dbdb7e941b0b6469e94290a2cd526816ada698c3
r0mg <roslynnmg@gmail.com>
2015-11-23 18:42:02 -0500

    done

---
+++ app/routes/artists.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(){
+    return [Ember.Object.create({name: "Kanye West"}), Ember.Object.create({name: "Lady Gaga"})];
+   },
+
+  actions: {
+    addArtist: function(artistName) {
+      let model = this.modelFor(this.routeName);
+      var newArtist = Ember.Object.create({name: artistName});
+      model.pushObject(newArtist);
+      this.controller.set('artistName', '');
+      return model;
+    }
+  }
+});
